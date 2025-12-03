759d060a09e4890d609d2929f00579b2e921b93c
venkatamar21 <venkatamar21@gmail.com>
2025-12-03 06:27:11 +0000

    Added First custom JS action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,5 +1,8 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+const exec = require('@actions/exec')
 function run(){
-    console.log();
+    core.notice(' Hello from my custom Javascript Action')
     
 }
 
